af786e86362e702f1b1d06f766ae8d7f7e4910dc
Arnaud (Limayrac) <arnaud.rabhi@limayrac.fr>
2023-12-05 16:19:31 +0100

    Init Spring project

---
+++ src/main/java/fr/limayrac/declarationFrais/declarationFrais/DeclarationFraisApplication.java
@@ -0,0 +1,13 @@
+package fr.limayrac.declarationFrais.declarationFrais;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeclarationFraisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeclarationFraisApplication.class, args);
+	}
+
+}
